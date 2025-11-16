6349617197171e082221593097e56622cd126949
vitorbite <vitorbitencourt20@gmail.com>
2025-11-15 21:48:09 -0300

    Removendo comentários desnecessários

--- src/br/edu/icev/aed/forense/MinhaAnaliseForense.java
+++ src/br/edu/icev/aed/forense/MinhaAnaliseForense.java
@@ -8,17 +8,10 @@ import br.edu.icev.aed.forense.AnaliseForenseAvancada;
 
 public class MinhaAnaliseForense implements AnaliseForenseAvancada {
 
-    // public List<String[]> LerArquivo(String caminhoArquivo) {
-    Alerta alerta = new Alerta(0, null, null, null, null, 0, 0);
-
     @Override
-    public Set<String> encontrarSessoesInvalidas(String arquivo)
-    // throws IOException
-    {
+    public Set<String> encontrarSessoesInvalidas(String arquivo){
         Set<String> logins_invalidos = new TreeSet<>();
         Map<String, Stack<String>> mapa = new TreeMap<>();
-
-        // List<String[]> linhas = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
             String linha;
@@ -28,31 +21,26 @@ public class MinhaAnaliseForense implements AnaliseForenseAvancada {
                     primeiraExec = false;
                     continue;                    
                 }
-                // Divide a linha por vírgulas, respeitando o formato CSV simples
                 String[] valores = linha.split(",");
-                // linhas.add(valores);
 
-                // Valida se a linha tem todos os campos necessários
                 if (valores.length < 7) {
                     continue;
                 }
 
-                Alerta alerta = new Alerta(Long.parseLong(valores[0]), valores[1], valores[2], valores[3], valores[4], Integer.parseInt(valores[5]), Long.parseLong(valores[6].isEmpty() ? "0" : valores[6]));
+                Alerta alerta = new Alerta(Long.parseLong(valores[0]), valores[1], valores[2], valores[3], valores[4], Integer.parseInt(valores[5]), Long.parseLong(valores[6]));
                 Stack<String> pilha = mapa.computeIfAbsent(alerta.getUserId(), (k) -> new Stack<>());
 
                 if (mapa.containsKey(alerta.getUserId())) {
                     if (alerta.getActionType().equals("LOGIN")) {
                         pilha.push(alerta.getSessionId());
-                        // System.out.println(mapa);
-                        // System.out.println("\n\n");
-                        // System.out.println(pilha);
+
                     } else if (alerta.getActionType().equals("LOGOUT")) {
                         if (pilha.empty()) {
                             logins_invalidos.add(alerta.getSessionId());
-                            // System.out.println(pilha);
+
                         }else{
                             String sessao = pilha.pop();
-                            // System.out.println(pilha);
+
                             if (!sessao.equals(alerta.getSessionId())) {
                                 logins_invalidos.add(sessao);
                                 logins_invalidos.add(alerta.getSessionId());
@@ -61,15 +49,10 @@ public class MinhaAnaliseForense implements AnaliseForenseAvancada {
 
                     }
                 }
-                // System.out.println(logins_invalidos);
-                // mapa.put(valores[1], new Stack<String>());
             }
         } catch (IOException e) {
             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
-        } // Implementar usando Map<String, Stack<String>>
-        // if (logins_invalidos == null) {
-        //     return new HashSet<>();
-        // }
+        } 
         System.out.println(logins_invalidos);
         return logins_invalidos.isEmpty() ? new HashSet<>() : logins_invalidos;
     }

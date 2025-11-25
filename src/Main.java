332be9c9f10617222438ccaa79d10cd198c7cc7a
vitorbite <vitorbitencourt20@gmail.com>
2025-11-24 21:08:47 -0300

    Implementando o Desafio 5: Método rastrear Contaminação

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-import br.edu.icev.aed.forense.Alerta;
 import br.edu.icev.aed.forense.MinhaAnaliseForense;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         MinhaAnaliseForense a = new MinhaAnaliseForense();
-        // a.encontrarSessoesInvalidas(...);
-        a.encontrarPicosTransferencia("C:\\Users\\arthu\\OneDrive\\Desktop\\AED2\\Trabalho_AED2\\arquivo_logs.csv");
+        // a.encontrarSessoesInvalidas("arquivo_logs.csv");
+        // a.encontrarPicosTransferencia("arquivo_logs.csv");
         try {
            
-            var alertas = a.priorizarAlertas("C:\\Users\\arthu\\OneDrive\\Desktop\\AED2\\Trabalho_AED2\\arquivo_logs.csv", 4);
-            
+        // a.priorizarAlertas("arquivo_logs.csv", 4);
+        a.rastrearContaminacao("arquivo_logs.csv", "/usr/bin/sshd", "/usr/bin/telnetd");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
